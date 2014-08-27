bbd334f0df3e4c6c749eb0a017a5abe5b14e3411
Charles Bergeron <chuck.bergeron@gmail.com>
2014-08-27 16:29:09 -0700

    touch up flash messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
+
   def index
     @vote = Vote.new
   end
+
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,11 @@
 class VotesController < ApplicationController
+
   def create
     @vote = Vote.new(vote_params)
     @vote.ip_address = request.remote_ip
+
     if @vote.save
-      redirect_to :root, notice: "Thanks for voting!"
+      redirect_to :root, notice: "We've got your submission. Thanks for voting!"
     else
       render "home/index"
     end
@@ -14,4 +16,5 @@ class VotesController < ApplicationController
   def vote_params
     params.require(:vote).permit(:voter_id, :nominee_id, :reason)
   end
+
 end
