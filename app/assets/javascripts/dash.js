6b0e60df489ba8ab1b5fe95d10bc25524c0e3d40
Charles Bergeron <chuck.bergeron@gmail.com>
2014-08-27 18:16:30 -0700

    animations on dash almost working great, need to slide the face thumb up

--- app/assets/javascripts/dash.js
+++ app/assets/javascripts/dash.js
@@ -1,28 +1,43 @@
+jQuery.fn.random = function() {
+  var randomIndex = Math.floor(Math.random() * this.length);
+  return jQuery(this[randomIndex]);
+};
+
 $( document ).ready(function() {
 
   (function() {
 
     if($('.dash')){
 
-      var timeTilPageReloads = 15000;
-      var timeTilHighlightNominee = 2000;
-      var highlightDuration = 10000;
+      var timeTilPageReloads      = 15000,
+          timeTilHighlightNominee = 800,
+          highlightDuration       = 20000;
+
+      var $nominee = $('td.nominee').random();
+      $('.reason-bridge').html($nominee.find('.hidden').html());
 
       function reloadPage(){
         return window.location.reload(true);
       }
 
-      function stopHighlight(){
-        $('.overlay').removeClass('visible');
-        $('#github_id_pcreux').removeClass('visible');
-      }
-
       function startHighlight(){
         $('.overlay').addClass('visible');
-        $('#github_id_pcreux').addClass('visible');
+        $nominee.addClass('visible');
+
+        $('.dash table').addClass('slide-down');
+
+        $('.reason-bridge').addClass('visible');
+        setTimeout(function(){
+          $('.reason-bridge').removeClass('hidden');
+        }, 2000);
 
         setTimeout(stopHighlight, highlightDuration);
         setTimeout(reloadPage, timeTilPageReloads);
+      }
+
+      function stopHighlight(){
+        $('.overlay').removeClass('visible');
+        $nominee.removeClass('visible');
       }
 
       setTimeout(startHighlight, timeTilHighlightNominee);
